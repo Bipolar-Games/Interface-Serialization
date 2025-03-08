33bebf7034f88b95330440287b59d203f4a5c4d6
Kosmik <malpa.w.klapkach@gmail.com>
2025-03-09 00:46:44 +0100

    Start working on custom object field
    In order to get rid of Interface selector button, but use default object selector button

--- Editor/InterfaceEditorGUI.cs
+++ Editor/InterfaceEditorGUI.cs
@@ -1,31 +1,106 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Bipolar.Editor
 {
 	public static class InterfaceEditorGUI
 	{
 		public const float InterfaceSelectorButtonWidth = 20;
-		
-		public static Object InterfaceField(Rect rect, GUIContent label, Object obj, System.Type interfaceType, bool allowSceneObjects)
+
+		private static int s_ObjectFieldHash = "s_ObjectFieldHash".GetHashCode();
+
+		private static GUIStyle buttonStyle = EditorStyles.objectField;
+
+		public static Object InterfaceField(Rect position, GUIContent label, Object @object, System.Type interfaceType, bool allowSceneObjects)
 		{
-			return default;
+			var currentEvent = Event.current;
+			var eventType = currentEvent.type;
+
+			int id = GUIUtility.GetControlID(hint: s_ObjectFieldHash, FocusType.Keyboard, position);
+			position = EditorGUI.PrefixLabel(position, id, label);
+
+			position = GetObjectFieldThumbnailRect(position, interfaceType);
+
+			var iconSize = EditorGUIUtility.GetIconSize();
+
+			//switch (eventType)
+			//{
+			//	case EventType.Repaint:
+
+			//		EditorGUIUtility.ObjectContent(@object, interfaceType);
+			//		Rect position2 = buttonStyle.margin.Remove(GetButtonRect(objectFieldVisualType, position));
+			//		buttonStyle.Draw(position2, GUIContent.none, id, DragAndDrop.activeControlID == id, position2.Contains(Event.current.mousePosition));
+			//		break;
+			//}
+			EditorGUIUtility.SetIconSize(iconSize);
+			return @object;
 		}
+
+
+
+
+
+		private static Rect GetObjectFieldThumbnailRect(Rect position, System.Type objType)
+		{
+			if (EditorGUIUtility.HasObjectThumbnail(objType) && position.height > 18f)
+			{
+				float size = Mathf.Min(position.width, position.height);
+				position.height = size;
+				position.xMin = position.xMax - size;
+			}
+
+			return position;
+		}
+
+
+
+
+
+
+
+
 
 		public static void InterfaceField(Rect rect, GUIContent label, SerializedProperty serializedObjectProperty, System.Type interfaceType, bool allowSceneObjects)
 		{
+			//var objectFieldRect = new Rect(rect.x, rect.y, rect.width - InterfaceSelectorButtonWidth, rect.height);
+			//var interfaceButtonRect = new Rect(rect.x + objectFieldRect.width, rect.y, InterfaceSelectorButtonWidth, rect.height);
+			//serializedObjectProperty.objectReferenceValue = EditorGUI.ObjectField(objectFieldRect, label, serializedObjectProperty.objectReferenceValue, interfaceType, allowSceneObjects);
+
 			if (interfaceType == default)
 				return;
 
-			var objectFieldRect = new Rect(rect.x, rect.y, rect.width - InterfaceSelectorButtonWidth, rect.height);
-			var interfaceButtonRect = new Rect(rect.x + objectFieldRect.width, rect.y, InterfaceSelectorButtonWidth, rect.height);
+			var currentEvent = Event.current;
+			var eventType = currentEvent.type;
+			int id = GUIUtility.GetControlID(hint: s_ObjectFieldHash, FocusType.Keyboard, rect);
 
-			serializedObjectProperty.objectReferenceValue = EditorGUI.ObjectField(objectFieldRect, label, serializedObjectProperty.objectReferenceValue, interfaceType, allowSceneObjects);
-			if (GUI.Button(interfaceButtonRect, "I"))
+			var @object = serializedObjectProperty.objectReferenceValue;
+
+
+			switch (eventType)
 			{
-				Object objectReferenceValue = serializedObjectProperty.objectReferenceValue;
-				InterfaceSelectorWindow.Show(interfaceType, objectReferenceValue, (obj) => AssignValue(serializedObjectProperty, obj));
+				case EventType.Repaint:
+						var tempContent = EditorGUIUtility.ObjectContent(serializedObjectProperty.objectReferenceValue, interfaceType);
+						buttonStyle.Draw(EditorGUI.IndentedRect(rect), tempContent, 1, DragAndDrop.activeControlID == id, rect.Contains(Event.current.mousePosition));
+
+						//	if (GUI.Button(interfaceButtonRect, "I"))
+						//	{
+						//		Object objectReferenceValue = serializedObjectProperty.objectReferenceValue;
+						//		InterfaceSelectorWindow.Show(interfaceType, objectReferenceValue, (obj) => AssignValue(serializedObjectProperty, obj));
+						//	}
+						break;
+
+				case EventType.DragExited:
+					if (GUI.enabled)
+						HandleUtility.Repaint();
+					
+					break;
 			}
+
+
+
+			AssignValue(serializedObjectProperty, @object);
+
 
 			static void AssignValue(SerializedProperty property, Object @object)
 			{
