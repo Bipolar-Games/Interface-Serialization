49feca2887e9d00c2307f6c79fb0a2d01af52fdd
Kosmik <malpa.w.klapkach@gmail.com>
2026-07-03 23:50:01 +0200

    Implement the Binding generator

---
+++ Source Generation/SourceGeneration~/BindSyntaxReceiver.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+
+namespace Bipolar.InterfaceSerialization.SourceGeneration
+{
+    internal class BindSyntaxReceiver : ISyntaxReceiver
+    {
+        public List<ClassDeclarationSyntax> CandidateClasses { get; } = new();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is ClassDeclarationSyntax classDecl
+                && classDecl.AttributeLists.Count > 0)
+            {
+                CandidateClasses.Add(classDecl);
+            }
+        }
+    }
+}
--- Source Generation/SourceGeneration~/SerializedInterfaceBindingGenerator.cs
+++ Source Generation/SourceGeneration~/SerializedInterfaceBindingGenerator.cs
@@ -2,19 +2,59 @@
 
 namespace Bipolar.InterfaceSerialization.SourceGeneration
 {
-    //[Generator]
+    public static class SerializedInterfaceBindingSettings
+    {
+        public const string AttributeFullName = "Bipolar.InterfaceSerialization.BindSerializedInterfaceAttribute";
+    }
+
+    [Generator]
     public class SerializedInterfaceBindingGenerator : ISourceGenerator
     {
-        private const string AttributeFullName = "Bipolar.InterfaceSerialization.GenerateSerializedClassAttribute";
-    
         public void Initialize(GeneratorInitializationContext context)
         {
-            throw new System.NotImplementedException();
+            context.RegisterForSyntaxNotifications(() => new BindSyntaxReceiver());
         }
 
         public void Execute(GeneratorExecutionContext context)
         {
-            throw new System.NotImplementedException();
+            if (context.SyntaxReceiver is not BindSyntaxReceiver receiver)
+                return;
+
+            foreach (var classSyntax in receiver.CandidateClasses)
+            {
+                var model = context.Compilation.GetSemanticModel(classSyntax.SyntaxTree);
+                if (model.GetDeclaredSymbol(classSyntax) is not INamedTypeSymbol classSymbol)
+                    continue;
+
+                if (TryGetAttribute(classSymbol, out var attribute) == false)
+                    continue;
+
+                if (attribute.ConstructorArguments[0].Value is not INamedTypeSymbol interfaceSymbol)
+                    continue;
+
+                var className = classSymbol.Name;
+                var interfaceName = interfaceSymbol.Name;
+                var namespaceName = classSymbol.ContainingNamespace?.ToDisplayString();
+
+                var source = SerializedInterfaceClassSourceGenerator.GenerateSource(namespaceName, className, interfaceName, interfaceSymbol, isPartial: true);
+                context.AddSource($"{className}.SerializedInterfaceBinding.g.cs", source);
+            }
+        }
+
+        private static bool TryGetAttribute(INamedTypeSymbol symbol, out AttributeData attribute)
+        {
+            var attributes = symbol.GetAttributes();
+            for (var i = 0; i < attributes.Length; i++)
+            {
+                if (attributes[i]?.AttributeClass?.ToDisplayString() == SerializedInterfaceBindingSettings.AttributeFullName)
+                {
+                    attribute = attributes[i];
+                    return true;
+                }
+            }
+
+            attribute = null!;
+            return false;
         }
     }
 }
