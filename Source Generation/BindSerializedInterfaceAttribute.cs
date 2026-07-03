db2fef026d4df5970b1ee5d8f6ca9f501ec57929
Kosmik <malpa.w.klapkach@gmail.com>
2026-07-03 22:52:56 +0200

    Create an attribute for binding

---
+++ Source Generation/BindSerializedInterfaceAttribute.cs
@@ -0,0 +1,17 @@
+#if UNITY_2022_1_OR_NEWER
+using System;
+
+namespace Bipolar.InterfaceSerialization
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class BindSerializedInterfaceAttribute : Attribute
+    {
+        public Type InterfaceType { get; }
+
+        public BindSerializedInterfaceAttribute(Type interfaceType)
+        {
+            InterfaceType = interfaceType;
+        }
+    }
+}
+#endif
