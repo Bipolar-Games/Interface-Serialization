4fb7bd98dfe2b7afad7b6d27c60620d3434433ec
Kosmik <malpa.w.klapkach@gmail.com>
2026-07-03 22:34:31 +0200

    Separate generator classes into files

---
+++ Source Generation/SourceGeneration~/InterfacesWithAttributesSyntaxReceiver.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+
+namespace Bipolar.InterfaceSerialization.SourceGeneration
+{
+    internal class InterfacesWithAttributesSyntaxReceiver : ISyntaxReceiver
+    {
+        public List<InterfaceDeclarationSyntax> CandidateInterfaces { get; } = new List<InterfaceDeclarationSyntax>();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is InterfaceDeclarationSyntax interfaceDecl && interfaceDecl.AttributeLists.Count > 0)
+            {
+                CandidateInterfaces.Add(interfaceDecl);
+            }
+        }
+    }
+}
---
+++ Source Generation/SourceGeneration~/SerializedInterfaceBindingGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Bipolar.InterfaceSerialization.SourceGeneration
+{
+    //[Generator]
+    public class SerializedInterfaceBindingGenerator : ISourceGenerator
+    {
+        private const string AttributeFullName = "Bipolar.InterfaceSerialization.GenerateSerializedClassAttribute";
+    
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
